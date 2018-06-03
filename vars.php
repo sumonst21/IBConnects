fd7983aec75fb56d09ab8c00a6ea7495e78d10c4
Md. Sumon Islam <developer.sumon@gmail.com>
2018-06-04 03:01:15 +0600

    updated vars

--- vars.php
+++ vars.php
@@ -1,3 +1,9 @@
 <?php
 $siteurl = get_site_url();
+$phpself = $_SERVER['PHP_SELF'];
+$servname = $_SERVER['SERVER_NAME'];
+$host = $_SERVER['HTTP_HOST'];
+$htreferer = $_SERVER['HTTP_REFERER'];
+$htuser_agnet = $_SERVER['HTTP_USER_AGENT'];
+$scriptname = $_SERVER['SCRIPT_NAME'];
 ?>
